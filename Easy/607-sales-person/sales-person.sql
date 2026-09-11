a14e85d94fa514a3b5e66a6744d5efbe54107b9f
Sunesh Krishna N <suneshkarun@gmail.com>
2026-09-11 20:00:28 +0530

    Time: 1862 ms (21.61%) | Memory: 0B (100.00%) - LeetSync

--- Easy/607-sales-person/sales-person.sql
+++ Easy/607-sales-person/sales-person.sql
@@ -1,9 +1,1 @@
-SELECT s.name
-FROM SalesPerson s
-WHERE s.sales_id NOT IN (
-    SELECT o.sales_id
-    FROM Orders o
-    JOIN Company c
-      ON o.com_id = c.com_id
-    WHERE c.name = 'RED'
-);
+SELECT s.name FROM SalesPerson s WHERE s.sales_id NOT IN(SELECT sales_id FROM Orders WHERE com_id IN(SELECT com_id FROM Company WHERE name='RED'));
